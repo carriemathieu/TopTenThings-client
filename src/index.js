3a95f894f93b6c0a2c29aa12106a19ce6a81b83b
Carrie Mathieu <carrieburks@gmail.com>
2021-05-17 22:20:43 -0400

    installed redux, react-redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import { thunk } from 'redux-thunk'
 
 import App from './components/App'
 
