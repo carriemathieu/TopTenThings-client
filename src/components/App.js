8c6c1b92550932f96633e2c2c5bb17670190ad8a
Carrie Mathieu <carrieburks@gmail.com>
2021-05-17 22:15:41 -0400

    commented out fetch code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 class App extends React.Component {
-    componentDidMount() {
-        fetch("http://localhost:3000/api/v1/categories")
-            .then(resp => resp.json)
-            .then(res => console.log(res))
-    }
+    // componentDidMount() {
+    //     fetch("http://localhost:3000/api/v1/categories")
+    //         .then(resp => resp.json())
+    //         .then(console.log)
+    // }
 
     render() {
         return ( 
