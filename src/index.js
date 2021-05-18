33c03e87ab6a6fbe6bc56a926e4262751e309c5e
Carrie Mathieu <carrieburks@gmail.com>
2021-05-17 22:52:28 -0400

    move store to own file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
-import { thunk } from 'redux-thunk'
 
+import store from './store'
 import App from './components/App'
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)) )
 
 ReactDOM.render(
     <Provider store= {store}>
