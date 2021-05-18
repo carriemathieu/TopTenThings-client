f6cb6dd182b035aea7006e41d496cbb2baa0efb3
Carrie Mathieu <carrieburks@gmail.com>
2021-05-17 22:25:31 -0400

    composeEnhancer / store

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import { thunk } from 'redux-thunk'
 
 import App from './components/App'
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION__ || compose
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)) )
+
 ReactDOM.render(
-    <App />,
+    <Provider store= {store}>
+    <App />
+    </Provider>,
     document.querySelector('#root')
 )
