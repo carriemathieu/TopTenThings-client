4ef831cca0ce45f0fe4a330129343f5915a0443b
Carrie Mathieu <carrieburks@gmail.com>
2021-05-18 22:20:37 -0400

    login action/fetch

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -7,6 +7,12 @@ export const setCurrentUser = user => {
 
 export const login = credentials => {
     return dispatch => {
-        return fetch("http://localhost:3000")
+        return fetch("http://localhost:3000/api/v1/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(credentials)
+        })
     }
 }
