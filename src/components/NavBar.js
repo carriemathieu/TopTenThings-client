7793f2f3315712732cb7f0dd23ea19f37c3cc603
Carrie Mathieu <carrieburks@gmail.com>
2021-05-23 16:41:57 -0400

    Navbar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Login from './Login.js'
+import Logout from './Logout.js'
+import { connect } from 'react-redux'
+
+// functional component so no "this" -> just props, currentUser destructured from props
+const NavBar = ({ currentUser }) => {
+    return (
+        <div>
+            {currentUser ? `Welcome ${currentUser.first_name}` : ""}
+            {currentUser ? <Logout /> : <Login/>}
+        </div>
+    )
+}
+
+const mapStateToProps = ({ currentUser }) => {
+    return {
+        currentUser
+    }
+}
+
+export default connect(mapStateToProps)(NavBar)
