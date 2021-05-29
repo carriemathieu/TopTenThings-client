a18ab49537cd409dd7471cced21b07814c109cf7
Carrie Mathieu <carrieburks@gmail.com>
2021-05-28 22:09:13 -0400

    imported allLists to store & combine reducers

--- src/store.js
+++ src/store.js
@@ -2,12 +2,14 @@ import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import users from './reducers/users.js'
 import currentUser from './reducers/currentUser.js'
 import loginForm from './reducers/loginForm'
+import allLists from './reducers/allLists'
 import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
     users, 
     currentUser,
-    loginForm
+    loginForm,
+    allLists
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
