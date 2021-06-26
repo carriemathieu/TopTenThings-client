f9f48a6658722b29b2715fdb2ab64f86d3c4d977
Carrie Mathieu <carrieburks@gmail.com>
2021-06-26 11:10:33 -0400

    removed some logic - may add back later

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Login from './Login.js'
-import Logout from './Logout.js'
+// import Login from './Login.js'
+// import Logout from './Logout.js'
 import { connect } from 'react-redux'
 
 // functional component so no "this" -> just props, currentUser destructured from props
@@ -8,9 +8,6 @@ const NavBar = ({ currentUser }) => {
     return (
         <div className = "Navbar">
             {currentUser ? `Welcome ${currentUser.attributes.first_name}` : ""}
-            <button>Log in</button> 
-            or
-            <button>Create an Account</button>
             {/* {currentUser ? <Logout /> : <Login/>} */}
         </div>
     )
