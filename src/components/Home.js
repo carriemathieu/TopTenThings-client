24249b49caa159b14f5c816ae5c52fa7c097754c
Carrie Mathieu <carrieburks@gmail.com>
2021-06-26 11:56:10 -0400

    create home component w/ login & sign up links

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './Login.js'
+import SignUp from './SignUp'
+import { Link } from 'react-router-dom'
+
+const Home = ({}) => {
+    return (
+        <div>
+            Welcome! Please <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link>!
+        </div>
+    )
+}
+
+export default Home
