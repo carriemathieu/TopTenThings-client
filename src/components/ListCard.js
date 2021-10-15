61840db7f4bf8c531c8abc55dbf597015fb7630e
Carrie Mathieu <carrieburks@gmail.com>
2021-10-14 22:25:31 -0400

    individual list show page - update styling

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -4,26 +4,35 @@ import { Card, ListGroup } from 'react-bootstrap'
 const ListCard = ({list}) => {
     return (
         list ? 
-        <Card
-            bg='dark'
-            text='white'
-            style={{ width: '18rem' }}
-            className="mb-2"
-        >
-            <Card.Body>
-                <Card.Title>{list.attributes.list_title}</Card.Title>
-                <Card.Text>
+        <div className="listContainer">
+            <Card
+                bg='dark'
+                text='white'
+                className="mb-2"
+            >
+                <Card.Body>
+                    <Card.Title className="text-center">{list.attributes.list_title}</Card.Title>
+                    
+                </Card.Body>
+
+                <Card.Text className="text-center">
                     Category: {list.attributes.category.name}
-                    {/* <ListGroup variant="flush"> */}
-                        {console.log("list",list.attributes.list_content)}
-                    {/* </ListGroup> */}
                     
                 </Card.Text>
-                <footer className="blockquote-footer">Submitted by: {list.attributes.user.first_name}</footer>
-                <Card.Link href={`/lists/${list.id}/edit`}>Edit this list</Card.Link>
-            </Card.Body>
-        </Card>
-        : <p>  </p>
+
+                <ListGroup variant="flush">
+                    {list.attributes.list_content.map((listItem, index) => <ListGroup.Item key={index}>{listItem}</ListGroup.Item>)}
+                </ListGroup>
+                    
+                <Card.Footer className="text-center">
+                    <small className="text-muted">Submitted by: {list.attributes.user.first_name}</small>
+                </Card.Footer>
+
+                <Card.Link href={`/lists/${list.id}/edit`} className="text-center" id="edit-list-link">Edit this list</Card.Link>
+                
+            </Card>
+        </div>
+        : null
     )
 }
 
